73657e707113089def4b3f03f2e87b26c64525f2
RevCameron <95146137+RevCameron@users.noreply.github.com>
2026-03-09 17:03:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -318,11 +318,6 @@ function getPodSizes(playerCount) {
 function renderRoundTabs() {
   const tabs = document.getElementById("roundTabs");
   tabs.innerHTML = "";
-  // Conditional Column Logic
-  const podCount = round.pods.length;
-  pairingsSection.style.display = "grid";
-  pairingsSection.style.gap = "20px";
-  pairingsSection.style.alignItems = "start";
 
   if (podCount === 2 || podCount === 4) {
     pairingsSection.style.gridTemplateColumns = "1fr 1fr"; // 2 columns
